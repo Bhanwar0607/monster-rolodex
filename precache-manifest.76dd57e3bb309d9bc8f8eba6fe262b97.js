18a4cee43c4d9e4a250c0f7bab80b66814c915e0
bhanwar0607 <bhavar1011@gmail.com>
2019-08-22 23:27:06 +0530

    Updates

---
+++ precache-manifest.76dd57e3bb309d9bc8f8eba6fe262b97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbc39d49175eeb02becf72a009b6a5b",
+    "url": "/Bhanwar0607/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b491e74bfda4c1f286fb",
+    "url": "/Bhanwar0607/monster-rolodex/static/css/main.ec31570a.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/Bhanwar0607/monster-rolodex/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "b491e74bfda4c1f286fb",
+    "url": "/Bhanwar0607/monster-rolodex/static/js/main.8bea54aa.chunk.js"
+  },
+  {
+    "revision": "a3677f7f4980f678cbc7",
+    "url": "/Bhanwar0607/monster-rolodex/static/js/runtime~main.c3765b76.js"
+  }
+]);
